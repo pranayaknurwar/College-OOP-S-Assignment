b000e886e283b0339cb753f9708e691726471d0b
pranayaknurwar <aknurwarpranay@gmail.com>
2026-02-04 22:06:51 +0530

    feat : add class Animal and its methods

---
+++ Polymorphism/poly.java
@@ -0,0 +1,24 @@
+package Polymorphism;
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+
