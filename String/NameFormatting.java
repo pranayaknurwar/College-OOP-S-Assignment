1b9b2885b9ea1edc1ba99a8eaae2d7004f945fc3
pranayaknurwar <aknurwarpranay@gmail.com>
2026-02-06 23:56:04 +0530

    feat : Name validation

---
+++ String/NameFormatting.java
@@ -0,0 +1,9 @@
+public class NameFormatting {
+    public static void main(String[] args) {
+
+        String customerName = "Akash Sharma";
+
+        System.out.println("Upper Case : " + customerName.toUpperCase());
+        System.out.println("Lower Case : " + customerName.toLowerCase());
+    }
+}
