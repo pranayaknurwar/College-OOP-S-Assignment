8fc187c7f66597651083154fb76fe01e63326801
pranayaknurwar <aknurwarpranay@gmail.com>
2026-02-08 23:50:10 +0530

    feat: implement runtime polymorphism for multiple payment methods

---
+++ Overriding/PaymentApp.java
@@ -0,0 +1,33 @@
+package Overriding;
+
+class Payment {
+    void processPayment() {
+        System.out.println("Processing generic payment");
+    }
+}
+
+class UpiPayment extends Payment {
+    @Override
+    void processPayment() {
+        System.out.println("Processing UPI payment");
+    }
+}
+
+class CardPayment extends Payment {
+    @Override
+    void processPayment() {
+        System.out.println("Processing Card payment");
+    }
+}
+
+public class PaymentApp {
+    public static void main(String[] args) {
+        Payment payment;
+
+        payment = new UpiPayment();
+        payment.processPayment();
+
+        payment = new CardPayment();
+        payment.processPayment();
+    }
+}
